843bf8351bbd266d1f177329155016d48b3bec0b
flamingo <zy06112911@163.com>
2023-03-07 14:20:58 +0800

    fix print and format

--- code/train_alexnet.py
+++ code/train_alexnet.py
@@ -20,27 +20,60 @@ model_urls = {
     'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
 }
 
+
 def parse_args():
     """Parse input arguments."""
-    parser = argparse.ArgumentParser(description='Head pose estimation using the Hopenet network.')
-    parser.add_argument('--gpu', dest='gpu_id', help='GPU device id to use [0]',
-            default=0, type=int)
-    parser.add_argument('--num_epochs', dest='num_epochs', help='Maximum number of training epochs.',
-          default=5, type=int)
-    parser.add_argument('--batch_size', dest='batch_size', help='Batch size.',
-          default=16, type=int)
-    parser.add_argument('--lr', dest='lr', help='Base learning rate.',
-          default=0.001, type=float)
-    parser.add_argument('--data_dir', dest='data_dir', help='Directory path for data.',
-          default='', type=str)
-    parser.add_argument('--filename_list', dest='filename_list', help='Path to text file containing relative paths for every example.',
-          default='', type=str)
-    parser.add_argument('--output_string', dest='output_string', help='String appended to output snapshots.', default = '', type=str)
-    parser.add_argument('--alpha', dest='alpha', help='Regression loss coefficient.',
-          default=0.001, type=float)
-    parser.add_argument('--dataset', dest='dataset', help='Dataset type.', default='Pose_300W_LP', type=str)
+    parser = argparse.ArgumentParser(
+        description='Head pose estimation using the Hopenet network.')
+    parser.add_argument('--gpu',
+                        dest='gpu_id',
+                        help='GPU device id to use [0]',
+                        default=0,
+                        type=int)
+    parser.add_argument('--num_epochs',
+                        dest='num_epochs',
+                        help='Maximum number of training epochs.',
+                        default=5,
+                        type=int)
+    parser.add_argument('--batch_size',
+                        dest='batch_size',
+                        help='Batch size.',
+                        default=16,
+                        type=int)
+    parser.add_argument('--lr',
+                        dest='lr',
+                        help='Base learning rate.',
+                        default=0.001,
+                        type=float)
+    parser.add_argument('--data_dir',
+                        dest='data_dir',
+                        help='Directory path for data.',
+                        default='',
+                        type=str)
+    parser.add_argument(
+        '--filename_list',
+        dest='filename_list',
+        help='Path to text file containing relative paths for every example.',
+        default='',
+        type=str)
+    parser.add_argument('--output_string',
+                        dest='output_string',
+                        help='String appended to output snapshots.',
+                        default='',
+                        type=str)
+    parser.add_argument('--alpha',
+                        dest='alpha',
+                        help='Regression loss coefficient.',
+                        default=0.001,
+                        type=float)
+    parser.add_argument('--dataset',
+                        dest='dataset',
+                        help='Dataset type.',
+                        default='Pose_300W_LP',
+                        type=str)
     args = parser.parse_args()
     return args
+
 
 def get_ignored_params(model):
     # Generator function that yields ignored params.
@@ -51,6 +84,7 @@ def get_ignored_params(model):
                 module.eval()
             for name, param in module.named_parameters():
                 yield param
+
 
 def get_non_ignored_params(model):
     # Generator function that yields params that will be optimized.
@@ -66,6 +100,7 @@ def get_non_ignored_params(model):
             for name, param in module.named_parameters():
                 yield param
 
+
 def get_fc_params(model):
     b = [model.fc_yaw, model.fc_pitch, model.fc_roll]
     for i in range(len(b)):
@@ -73,12 +108,14 @@ def get_fc_params(model):
             for name, param in module.named_parameters():
                 yield param
 
+
 def load_filtered_state_dict(model, snapshot):
     # By user apaszke from discuss.pytorch.org
     model_dict = model.state_dict()
     snapshot = {k: v for k, v in snapshot.items() if k in model_dict}
     model_dict.update(snapshot)
     model.load_state_dict(model_dict)
+
 
 if __name__ == '__main__':
     args = parse_args()
@@ -94,28 +131,40 @@ if __name__ == '__main__':
     model = hopenet.AlexNet(66)
     load_filtered_state_dict(model, model_zoo.load_url(model_urls['alexnet']))
 
-    print 'Loading data.'
+    print('Loading data.')
 
-    transformations = transforms.Compose([transforms.Scale(240),
-    transforms.RandomCrop(224), transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+    transformations = transforms.Compose([
+        transforms.Scale(240),
+        transforms.RandomCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
 
     if args.dataset == 'Pose_300W_LP':
-        pose_dataset = datasets.Pose_300W_LP(args.data_dir, args.filename_list, transformations)
+        pose_dataset = datasets.Pose_300W_LP(args.data_dir, args.filename_list,
+                                             transformations)
     elif args.dataset == 'Pose_300W_LP_random_ds':
-        pose_dataset = datasets.Pose_300W_LP_random_ds(args.data_dir, args.filename_list, transformations)
+        pose_dataset = datasets.Pose_300W_LP_random_ds(args.data_dir,
+                                                       args.filename_list,
+                                                       transformations)
     elif args.dataset == 'AFLW2000':
-        pose_dataset = datasets.AFLW2000(args.data_dir, args.filename_list, transformations)
+        pose_dataset = datasets.AFLW2000(args.data_dir, args.filename_list,
+                                         transformations)
     elif args.dataset == 'BIWI':
-        pose_dataset = datasets.BIWI(args.data_dir, args.filename_list, transformations)
+        pose_dataset = datasets.BIWI(args.data_dir, args.filename_list,
+                                     transformations)
     elif args.dataset == 'AFLW':
-        pose_dataset = datasets.AFLW(args.data_dir, args.filename_list, transformations)
+        pose_dataset = datasets.AFLW(args.data_dir, args.filename_list,
+                                     transformations)
     elif args.dataset == 'AFLW_aug':
-        pose_dataset = datasets.AFLW_aug(args.data_dir, args.filename_list, transformations)
+        pose_dataset = datasets.AFLW_aug(args.data_dir, args.filename_list,
+                                         transformations)
     elif args.dataset == 'AFW':
-        pose_dataset = datasets.AFW(args.data_dir, args.filename_list, transformations)
+        pose_dataset = datasets.AFW(args.data_dir, args.filename_list,
+                                    transformations)
     else:
-        print 'Error: not a valid dataset name'
+        print('Error: not a valid dataset name')
         sys.exit()
     train_loader = torch.utils.data.DataLoader(dataset=pose_dataset,
                                                batch_size=batch_size,
@@ -132,25 +181,32 @@ if __name__ == '__main__':
     idx_tensor = [idx for idx in xrange(66)]
     idx_tensor = Variable(torch.FloatTensor(idx_tensor)).cuda(gpu)
 
-    optimizer = torch.optim.Adam([{'params': get_ignored_params(model), 'lr': 0},
-                                  {'params': get_non_ignored_params(model), 'lr': args.lr},
-                                  {'params': get_fc_params(model), 'lr': args.lr * 5}],
-                                   lr = args.lr)
+    optimizer = torch.optim.Adam([{
+        'params': get_ignored_params(model),
+        'lr': 0
+    }, {
+        'params': get_non_ignored_params(model),
+        'lr': args.lr
+    }, {
+        'params': get_fc_params(model),
+        'lr': args.lr * 5
+    }],
+                                 lr=args.lr)
 
-    print 'Ready to train network.'
+    print('Ready to train network.')
     for epoch in range(num_epochs):
         for i, (images, labels, cont_labels, name) in enumerate(train_loader):
             images = Variable(images).cuda(gpu)
 
             # Binned labels
-            label_yaw = Variable(labels[:,0]).cuda(gpu)
-            label_pitch = Variable(labels[:,1]).cuda(gpu)
-            label_roll = Variable(labels[:,2]).cuda(gpu)
+            label_yaw = Variable(labels[:, 0]).cuda(gpu)
+            label_pitch = Variable(labels[:, 1]).cuda(gpu)
+            label_roll = Variable(labels[:, 2]).cuda(gpu)
 
             # Continuous labels
-            label_yaw_cont = Variable(cont_labels[:,0]).cuda(gpu)
-            label_pitch_cont = Variable(cont_labels[:,1]).cuda(gpu)
-            label_roll_cont = Variable(cont_labels[:,2]).cuda(gpu)
+            label_yaw_cont = Variable(cont_labels[:, 0]).cuda(gpu)
+            label_pitch_cont = Variable(cont_labels[:, 1]).cuda(gpu)
+            label_roll_cont = Variable(cont_labels[:, 2]).cuda(gpu)
 
             # Forward pass
             pre_yaw, pre_pitch, pre_roll = model(images)
@@ -166,7 +222,8 @@ if __name__ == '__main__':
             roll_predicted = softmax(pre_roll)
 
             yaw_predicted = torch.sum(yaw_predicted * idx_tensor, 1) * 3 - 99
-            pitch_predicted = torch.sum(pitch_predicted * idx_tensor, 1) * 3 - 99
+            pitch_predicted = torch.sum(pitch_predicted * idx_tensor,
+                                        1) * 3 - 99
             roll_predicted = torch.sum(roll_predicted * idx_tensor, 1) * 3 - 99
 
             loss_reg_yaw = reg_criterion(yaw_predicted, label_yaw_cont)
@@ -183,12 +240,16 @@ if __name__ == '__main__':
             torch.autograd.backward(loss_seq, grad_seq)
             optimizer.step()
 
-            if (i+1) % 100 == 0:
-                print ('Epoch [%d/%d], Iter [%d/%d] Losses: Yaw %.4f, Pitch %.4f, Roll %.4f'
-                       %(epoch+1, num_epochs, i+1, len(pose_dataset)//batch_size, loss_yaw.data[0], loss_pitch.data[0], loss_roll.data[0]))
+            if (i + 1) % 100 == 0:
+                print(
+                    'Epoch [%d/%d], Iter [%d/%d] Losses: Yaw %.4f, Pitch %.4f, Roll %.4f'
+                    % (epoch + 1, num_epochs, i + 1,
+                       len(pose_dataset) // batch_size, loss_yaw.data[0],
+                       loss_pitch.data[0], loss_roll.data[0]))
 
         # Save models at numbered epochs.
         if epoch % 1 == 0 and epoch < num_epochs:
-            print 'Taking snapshot...'
-            torch.save(model.state_dict(),
-            'output/snapshots/' + args.output_string + '_epoch_'+ str(epoch+1) + '.pkl')
+            print('Taking snapshot...')
+            torch.save(
+                model.state_dict(), 'output/snapshots/' + args.output_string +
+                '_epoch_' + str(epoch + 1) + '.pkl')
